115bbea20c3572836f3d786303d185dd8563f1e9
Brian Bugh <brainbag@gmail.com>
2021-06-23 09:12:57 -0500

    Add stylelint standard

---
+++ stylelint.config.js
@@ -0,0 +1,21 @@
+// "lint": "concurrently -c 'bgBlue.bold,bgMagenta.bold' -p \"[{name}]\" -n \"code,style\" \"yarn:lint:*\"",
+
+module.exports = {
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: [
+          "tailwind",
+          "apply",
+          "variants",
+          "responsive",
+          "screen",
+        ],
+      },
+    ],
+    "declaration-block-trailing-semicolon": null,
+    "no-descending-specificity": null,
+  },
+};
