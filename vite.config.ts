2ee2e3145df407ccf66fe1f26855585f65f5f34e
Brian Bugh <brainbag@gmail.com>
2021-06-24 12:34:14 -0500

    Switch to vite-plugin-ts-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import Checker from "vite-plugin-checker";
+import TsChecker from "vite-plugin-ts-checker";
 import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx({}), Checker({ typescript: true }), WindiCSS()],
+  plugins: [vue(), vueJsx({}), TsChecker(), WindiCSS()],
 });
